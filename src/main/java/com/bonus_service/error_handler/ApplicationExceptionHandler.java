f574c9e0474f4aad6cb9212248b9929cff669524
Denis Milovanov <milovanovda@gmail.com>
2023-01-30 12:13:14 -0800

    added human-readable message in case of EmptyResultDataAccessException

--- src/main/java/com/bonus_service/error_handler/ApplicationExceptionHandler.java
+++ src/main/java/com/bonus_service/error_handler/ApplicationExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.bonus_service;
+package com.bonus_service.error_handler;
 
 
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -8,13 +8,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException ex) {
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        ErrorResponse error = new ErrorResponse(Arrays.asList("Data you requested not found"));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 
